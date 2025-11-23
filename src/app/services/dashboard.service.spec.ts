186ff8ff7cef36ddf37c3823e19108a27663dd28
AlcinaMoon <alcinamoon@gmail.com>
2025-11-23 12:00:00 -0300

    finalmente acabou, amem irmoes

--- src/app/services/dashboard.service.spec.ts
+++ src/app/services/dashboard.service.spec.ts
@@ -1,16 +1,21 @@
+import { DashboardService } from './dashboard.service';
 import { TestBed } from '@angular/core/testing';
 
-import { DashboardService } from './dashboard.service';
-
 describe('DashboardService', () => {
+
   let service: DashboardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      // configuração vazia, como no arquivo original
+    });
+
     service = TestBed.inject(DashboardService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    const instance = service;
+    expect(instance).toBeTruthy();
   });
+
 });
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,24 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, model } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CarroVin } from '../utils/carroVinInterface';
-import { VeiculosAPI} from '../models/veiculo.model'
 
+import { CarroVin } from '../utils/carroVinInterface';
+import { VeiculosAPI } from '../models/veiculo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
+  
   private baseUrl = 'http://localhost:3001';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
-  getVehicles(): Observable<VeiculosAPI>{
-    return this.http.get<VeiculosAPI> (`${this.baseUrl}/vehicles`)
+  getVehicles(): Observable<VeiculosAPI> {
+    const endpoint = `${this.baseUrl}/vehicles`;
+    return this.http.get<VeiculosAPI>(endpoint);
   }
-  buscarVin(codigoVin: string): Observable<CarroVin>{
-    const reqVin = this.http.post<CarroVin>(`${this.baseUrl}/vehicleData`, {vin: codigoVin})
-    return reqVin
+
+
+  buscarVin(codigoVin: string): Observable<CarroVin> {
+    const endpoint = `${this.baseUrl}/vehicleData`;
+    const payload = { vin: codigoVin };
+
+    return this.http.post<CarroVin>(endpoint, payload);
   }
 }
